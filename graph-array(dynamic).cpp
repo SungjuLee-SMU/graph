cc1b90963ba61da1bad5fa00975cb50c89249a06
202021025@sangmyung.kr <202021025@sangmyung.kr>
2021-06-09 18:49:24 +0900

    [202021025 이정민] 데이터 구조 과제 제출합니다.

--- graph-array(dynamic).cpp
+++ graph-array(dynamic).cpp
@@ -8,21 +8,87 @@ typedef struct {
 } graphType;
 typedef graphType* graphPtr;
 
-
-int** InitMatrix(int NRow, int NCol, int Value)
-{
-	int** m;
-	m = new int*[NRow];
-
-	for (int i = 0; i < NRow; i++)
-		m[i] = new int[NCol];
-		for (int i = 0; i < NRow; i++)
-			for (int j = 0; j < NCol; j++)
-				m[i][j] = Value;
-		return m;
+graphPtr Create(int V) {
+	graphPtr g = new graphType;
+	g->E = 0;
+	g->V = 0;
+	g->M = InsertVertex(V, V, 0);
+	return g;
 }
 
-void FreeMatrix(int NRow, int **m)
+int** InsertVertex(int NRow, int NCol, int Value) {
+	int** m;
+	m = new int* [NRow];
+
+	for (int i = 0; i < NRow; i++) {
+		m[i] = new int[NCol];
+		for (int i = 0; i < NRow; i++) {
+			for (int j = 0; j < NCol; j++)
+				m[i][j] = Value;
+		}
+		return m;
+	}
+}
+
+void InsertEdge(graphPtr g, int V1, int V2, int weight)
+{
+	if (g->M[V1][V2] == 0) {
+		g->E++;
+		g->M[V1][V2] = weight;
+		g->M[V2][V1] = weight;
+	}
+}
+
+void DeleteVertex(graphPtr g, int V1) {
+	for (int i = 0; i < g->V; i++) {
+		g->M[V1][i] = 0;
+		g->M[i][V1] = 0;
+	}
+}
+
+void DeleteEdge(graphPtr g, int V1, int V2) {
+	if (g->M[V1][V2] != 0) {
+		g->E--;
+		g->M[V1][V2] = 0;
+		g->M[V2][V1] = 0;
+	}
+}
+
+void RetrieveVertex(graphPtr g, int Value) {
+	for (int i = 0; i < g->V; i++) {
+		for (int j = 0; j < g->V; j++) {
+			if (g->M[i][j] == Value) {
+				cout << "찾음" << Value << endl;
+			}
+			else
+				cout << "찾지 못함" << endl;
+		}
+	}
+}
+
+void IsAdjacent(graphPtr g, int V1, int V2) {
+	for (int i = 0; i < g->V; i++) {
+		if (g->M[V1][i] != V2) {
+			cout << "IsAdjacent" << endl;
+		}
+		else
+			cout << "NotAdjacent" << endl;
+	}
+}
+
+void IsEmpty(graphPtr g) {
+	for (int i = 0; i < g->V; i++) {
+		for (int j = 0; j < g->V; j++) {
+			if (g->M[i][j] != 0) {
+				cout << "IsEmpty" << endl;
+			}
+			else
+				cout << "NotEmpty" << endl;
+		}
+	}
+}
+
+void Destroy(int NRow, int **m)
 {
 	for(int i = 0; i < NRow; i++)
 		delete(m[i]);
@@ -34,24 +100,14 @@ graphPtr InitGraph(int V)
 	graphPtr G = new graphType;
 	G->V = V;
 	G->E = 0;
-	G->M = InitMatrix(V, V, 0);
+	G->M = InsertVertex(V, V, 0);
 	return G;
 }
-
-void InsertEdge(graphPtr g, int V1, int V2, int weight)
-{
-  if (g->M[V1][V2] == 0){
-	  g->E++;
-	  g->M[V1][V2] = weight;
-	  g->M[V2][V1] = weight;
-  }
-}
-
 
 int main()
 {
 	graphPtr GP=NULL;
-	GP = InitGraph(6);
+	GP = Create(6);
 	InsertEdge(GP, 0, 1, 12);
 	InsertEdge(GP, 0, 4, 5);
 	InsertEdge(GP, 1, 2, 3);
@@ -60,6 +116,8 @@ int main()
 	InsertEdge(GP, 3, 2, 4);
 	InsertEdge(GP, 4, 0, 5);
 	InsertEdge(GP, 4, 2, 8);
+	IsAdjacent(GP, 5, 2);
+	DeleteVertex(GP, 5);
 
 	return 0;
 }
